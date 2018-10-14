57622d37d58aa585a2a48f432fa30a297f4f80dc
Denise Lorenz <deniselorenz@Denises-MacBook-Pro.local>
2018-10-13 21:03:57 -0500

    Added all my code and styles, but script and images won't link.

---
+++ 4assets/4javascript/4game.js
@@ -0,0 +1,55 @@
+var targetNumber = "";
+var wins = 0;
+var losses = 0;
+var counter = 0;
+var images = ["/4assets/4images/oldmanclimber.jpg", "/4assets/4images/womanclimbing.jpg", "/4assets/4images/topofmtn.jpg", "/4assets/4images/repelclimb.jpg"];
+
+
+function randomTargetNumber() {
+    targetNumber = Math.floor(Math.random() * 120) + 19;  
+}
+
+function resetClimbers() {
+    for (var i = 0; i < images.length; i++) {
+        var climber = $("<img>");
+        climber.addClass("climber");
+        climber.attr("src", images[i]);
+        climber.attr("value", (Math.floor(Math.random() * 12) + 1));
+        climber.attr("height", "100");
+        $(".climber-images").append(climber);
+    }
+}
+
+function resetHTML() {
+    $(".target-number").html(targetNumber);
+    $(".wins-losses-counter").html("<p> Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
+    $(".score-number").html(counter);
+    $(".climber-images").empty();
+}
+
+function totalReset(){
+    randomTargetNumber();
+    counter = 0;
+    resetHTML();
+    resetClimbers();
+}
+
+randomTargetNumber();
+resetHTML ();
+resetClimbers();
+
+
+function climbersClick() {
+    counter += parseInt($(this).attr("value"));
+    $(".score-number").html(counter);
+    if (counter === targetNumber) {
+        wins++;
+        totalReset();
+    }
+    else if (counter > targetNumber) {
+        losses++;
+        totalReset();
+    };
+};
+
+$(document).on("click", ".climber", climbersClick);
